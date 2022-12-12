aa2effbb29906174d8973fd4d49df6d25942d57a
Jan Trejbal <jan@trejbal.land>
2022-12-12 17:39:41 +0100

    Net 7 (#71)
    * Update workflows
    
    * Update .Net
    
    * Update LangVersion
    
    * Add PayPal.props
    
    * Fix warnings

--- tests/PayPal.Sdk.Checkout.Test/Infrastructure/HttpClientLoggingScopeHttpMessageHandler.cs
+++ tests/PayPal.Sdk.Checkout.Test/Infrastructure/HttpClientLoggingScopeHttpMessageHandler.cs
@@ -34,9 +34,9 @@ namespace PayPal.Sdk.Checkout.Test.Infrastructure
 
             using var log = Log.BeginRequestPipelineScope(_scopeLogger, request);
 
-            await log.RequestPipelineStart(_requestLogger, request);
+            await log.RequestPipelineStart(_requestLogger, request, cancellationToken);
             var response = await base.SendAsync(request, cancellationToken);
-            await log.RequestPipelineEnd(_requestLogger, response);
+            await log.RequestPipelineEnd(_requestLogger, response, cancellationToken);
 
             return response;
         }
@@ -49,32 +49,34 @@ namespace PayPal.Sdk.Checkout.Test.Infrastructure
                 public static readonly EventId PipelineEnd = new(101, "RequestPipelineEnd");
             }
 
-            private static readonly Func<ILogger, HttpMethod, Uri?, string, IDisposable> BeginRequestPipelineScopeDefine = LoggerMessage.DefineScope<HttpMethod, Uri?, string>(
+            private static readonly Func<ILogger, HttpMethod, Uri?, string, IDisposable?> BeginRequestPipelineScopeDefine = LoggerMessage.DefineScope<HttpMethod, Uri?, string>(
                 "HTTP {HttpMethod} {Uri} {CorrelationId}"
             );
 
             private static readonly Action<ILogger, HttpMethod, Uri?, string, string?, Exception?> RequestPipelineStartDefine = LoggerMessage.Define<HttpMethod, Uri?, string, string?>(
                 LogLevel.Trace,
                 EventIds.PipelineStart,
-                "Start processing HTTP request {HttpMethod} {Uri} [Correlation: {CorrelationId}]: {HttpContent}"
+                "Start processing HTTP request {HttpMethod} {Uri} [Correlation: {CorrelationId}]: {HttpContent}",
+                options: new LogDefineOptions { SkipEnabledCheck = true }
             );
 
             private static readonly Action<ILogger, HttpStatusCode, string?, Exception?> RequestPipelineEndDefine = LoggerMessage.Define<HttpStatusCode, string?>(
                 LogLevel.Trace,
                 EventIds.PipelineEnd,
-                "End processing HTTP request - {StatusCode}: {HttpContent}"
+                "End processing HTTP request - {StatusCode}: {HttpContent}",
+                options: new LogDefineOptions { SkipEnabledCheck = true }
             );
 
-            private readonly IDisposable _loggerScope;
+            private readonly IDisposable? _loggerScope;
 
-            private Log(IDisposable loggerScope)
+            private Log(IDisposable? loggerScope)
             {
                 _loggerScope = loggerScope;
             }
 
             public void Dispose()
             {
-                _loggerScope.Dispose();
+                _loggerScope?.Dispose();
             }
 
             public static Log BeginRequestPipelineScope(ILogger logger, HttpRequestMessage request)
@@ -86,23 +88,29 @@ namespace PayPal.Sdk.Checkout.Test.Infrastructure
                 return log;
             }
 
-            public async Task RequestPipelineStart(ILogger logger, HttpRequestMessage request)
+            public async Task RequestPipelineStart(ILogger logger, HttpRequestMessage request, CancellationToken cancellationToken)
             {
-                var correlationId = GetCorrelationIdFromRequest(request);
-                string? content = null;
-                if (request.Content != null)
+                if (logger.IsEnabled(LogLevel.Trace))
                 {
-                    content = await GetContent(request.Content);
-                }
+                    var correlationId = GetCorrelationIdFromRequest(request);
+                    string? content = null;
+                    if (request.Content != null)
+                    {
+                        content = await GetContent(request.Content, cancellationToken);
+                    }
 
-                RequestPipelineStartDefine(logger, request.Method, request.RequestUri, correlationId, content, null);
+                    RequestPipelineStartDefine(logger, request.Method, request.RequestUri, correlationId, content, null);
+                }
             }
 
-            public async Task RequestPipelineEnd(ILogger logger, HttpResponseMessage response)
+            public async Task RequestPipelineEnd(ILogger logger, HttpResponseMessage response, CancellationToken cancellationToken)
             {
-                var content = await GetContent(response.Content);
+                if (logger.IsEnabled(LogLevel.Trace))
+                {
+                    var content = await GetContent(response.Content, cancellationToken);
 
-                RequestPipelineEndDefine(logger, response.StatusCode, content, null);
+                    RequestPipelineEndDefine(logger, response.StatusCode, content, null);
+                }
             }
 
             private static string GetCorrelationIdFromRequest(HttpRequestMessage request)
@@ -117,11 +125,12 @@ namespace PayPal.Sdk.Checkout.Test.Infrastructure
                 return correlationId;
             }
 
-            private async Task<string> GetContent(HttpContent httpContent)
+            private async Task<string> GetContent(HttpContent httpContent, CancellationToken cancellationToken)
             {
                 if (httpContent.Headers.ContentEncoding.Contains("gzip"))
                 {
-                    var stream = await httpContent.ReadAsStreamAsync();
+                    await httpContent.LoadIntoBufferAsync();
+                    var stream = await httpContent.ReadAsStreamAsync(cancellationToken);
                     if (!stream.CanSeek)
                     {
                         return "<Stream content unknown - can not seek>";
@@ -134,7 +143,11 @@ namespace PayPal.Sdk.Checkout.Test.Infrastructure
                     using var reader = new StreamReader(
                         compressionStream, Encoding.UTF8, detectEncodingFromByteOrderMarks: false, bufferSize: -1, leaveOpen: true
                     );
-                    var content = await reader.ReadToEndAsync();
+                    var content = await reader.ReadToEndAsync(
+#if NET7_0_OR_GREATER
+                        cancellationToken
+#endif
+                    );
 
                     stream.Seek(0, SeekOrigin.Begin);
 
@@ -150,7 +163,7 @@ namespace PayPal.Sdk.Checkout.Test.Infrastructure
                         sb.Append(contentDisposition);
                         sb.Append('\t');
 
-                        var content = await GetContent(innerContent);
+                        var content = await GetContent(innerContent, cancellationToken);
 
                         if (!string.IsNullOrEmpty(contentDisposition?.FileName))
                         {
@@ -165,7 +178,8 @@ namespace PayPal.Sdk.Checkout.Test.Infrastructure
 
                 if (httpContent is StreamContent streamHttpContent)
                 {
-                    var streamContent = await streamHttpContent.ReadAsStreamAsync();
+                    await httpContent.LoadIntoBufferAsync();
+                    var streamContent = await streamHttpContent.ReadAsStreamAsync(cancellationToken);
 
                     if (streamContent.CanSeek)
                     {
@@ -174,7 +188,11 @@ namespace PayPal.Sdk.Checkout.Test.Infrastructure
                         using var reader = new StreamReader(
                             streamContent, Encoding.UTF8, detectEncodingFromByteOrderMarks: false, bufferSize: -1, leaveOpen: true
                         );
-                        var stringContent = await reader.ReadToEndAsync();
+                        var stringContent = await reader.ReadToEndAsync(
+#if NET7_0_OR_GREATER
+                            cancellationToken
+#endif
+                        );
 
                         streamContent.Seek(0, SeekOrigin.Begin);
 
@@ -182,7 +200,7 @@ namespace PayPal.Sdk.Checkout.Test.Infrastructure
                     }
                 }
 
-                return await httpContent.ReadAsStringAsync();
+                return await httpContent.ReadAsStringAsync(cancellationToken);
             }
         }
     }
