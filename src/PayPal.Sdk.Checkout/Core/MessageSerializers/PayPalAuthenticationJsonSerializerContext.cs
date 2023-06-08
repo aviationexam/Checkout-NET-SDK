247803a1aaf20f52427b76f90b291b550edc7d4a
Jan Trejbal <jan@trejbal.land>
2023-06-08 15:11:33 +0200

    Use JsonSourceGenerationMode.Default (#92)

--- src/PayPal.Sdk.Checkout/Core/MessageSerializers/PayPalAuthenticationJsonSerializerContext.cs
+++ src/PayPal.Sdk.Checkout/Core/MessageSerializers/PayPalAuthenticationJsonSerializerContext.cs
@@ -8,7 +8,7 @@ namespace PayPal.Sdk.Checkout.Core.MessageSerializers;
 [JsonSourceGenerationOptions(
     WriteIndented = true,
     PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase,
-    GenerationMode = JsonSourceGenerationMode.Metadata
+    GenerationMode = JsonSourceGenerationMode.Default
 )]
 [JsonSerializable(typeof(AccessToken))]
 [JsonSerializable(typeof(RefreshToken))]
